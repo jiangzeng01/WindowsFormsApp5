ff599ef30ed6e7c127eea126fd6e028a5738418f
jiangzeng01 <49379637+jiangzeng01@users.noreply.github.com>
2020-08-28 14:57:49 +0800

    Init
    Init

---
+++ WindowsFormsApp5/Form1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp5
+{
+	public partial class Form1 : Form
+	{
+		private FormSettings frmSettings1 = new FormSettings();
+
+		public Form1()
+		{
+			InitializeComponent();
+		}
+
+		private void button1_Click(object sender, EventArgs e)
+		{
+			label1.Text = frmSettings1.FormText;
+		}
+	}
+}
---
+++ WindowsFormsApp5/FormSettings.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Configuration;
+
+namespace WindowsFormsApp5
+{
+    //Application settings wrapper class
+    sealed class FormSettings : ApplicationSettingsBase
+    {
+        [UserScopedSettingAttribute()]
+        public String FormText
+        {
+            get { return (String)this["FormText"]; }
+            set { this["FormText"] = value; }
+        }
+        protected override void OnSettingsLoaded(object sender, SettingsLoadedEventArgs e)
+        {
+            if (string.IsNullOrEmpty(FormText))
+            {
+                FormText = "override OnSettingsLoaded";
+            }
+        }
+    }
+}
